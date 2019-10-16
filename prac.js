ca50cfb1e69caf3c9bd1d74ed1bd5b585fcfffa3
Sunil Kudupudi <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2019-10-16 11:39:18 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,4 @@
+let name1,admin;
+name="sunil";
+admin=name;
+alert(admin);
---
+++ tasks.js
@@ -0,0 +1,17 @@
+
+let name="sunil";
+alert(`Helloo!! ${name}`);
+alert(name="" + 1 + 0);								//10
+alert(typeof(name));								//string
+alert(name="" - 1 + 0);								//-1
+alert(typeof(name));								//number
+alert(true + false);								//1
+alert("2" * "3");									//6
+alert(4 + 5 + "px");								//9px
+alert("$" + 4 + 5);									//$45
+alert("4" - 2);										//2
+alert("4px" - 2);									//NaN
+alert("  -9  " -5);									//-14
+alert(null + 1);									//1
+alert(undefined + 1);								//NaN
+alert(" \t \n" - 2);								//-2
